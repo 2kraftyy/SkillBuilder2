76192b7c32d6f89487da652a6c29cf4f0f8cbe76
Khalid Alexaneder <khalid.alexander2184@gmail.com>
2026-02-10 17:28:28 -0500

    sub

--- src/main/java/GameMaster.java
+++ src/main/java/GameMaster.java
@@ -1,3 +1,28 @@
 public class GameMaster {
+    private static int totalExperiencePoints;
+
+    public static void gainExperience(int x){
+        totalExperiencePoints += x;
+    }
+    public static int getTeamXP(){
+        return totalExperiencePoints;
+
+    }
+    public static void applyPowerUp(int xp){
+        totalExperiencePoints += xp;
+    }
+    public static void applyPowerUp(double multiplier){
+        totalExperiencePoints *= multiplier;
+    }
+    public static void resetGame(){
+        totalExperiencePoints = 0;
+    }
+    public static void main(String[] args){
+        gainExperience(100);
+        applyPowerUp(500);
+        applyPowerUp(2.5);
+        System.out.println("Total Experience Points: " + getTeamXP());
+
+    }
 
 }
